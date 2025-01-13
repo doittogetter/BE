16ff76def4857ac2bb8706165a0d5d0d7c08ce3b
top6543top <top6543top@gamil.com>
2025-01-14 01:36:24 +0900

    feat: 새로운 GPT 서비스 추가로 인한 클래스 이름 변경 Prompt -> KeyWordPrompt chatGPTService -> KeyWordChatGPTService

--- src/main/java/com/doittogether/platform/business/openai/KeyWordChatGPTService.java
+++ src/main/java/com/doittogether/platform/business/openai/KeyWordChatGPTService.java
@@ -3,6 +3,7 @@ package com.doittogether.platform.business.openai;
 import com.doittogether.platform.business.openai.dto.ChatGPTRequest;
 import com.doittogether.platform.business.openai.dto.ChatGPTResponse;
 import com.doittogether.platform.business.openai.util.TemplateUtil;
+import com.doittogether.platform.infrastructure.persistence.personality.PersonalityRepository;
 import com.doittogether.platform.presentation.dto.personality.PersonalityRequestDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class ChatGPTService {
+public class KeyWordChatGPTService {
     private final String model;
     private final String apiURL;
     private final RestTemplate template;
 
-    public ChatGPTService(
+    public KeyWordChatGPTService(
             @Value("${openai.model}") String model,
             @Value("${openai.api.url}") String apiURL,
             RestTemplate template
@@ -28,7 +29,7 @@ public class ChatGPTService {
     public ChatGPTResponse chat(final PersonalityRequestDto request) {
         String personalizedQuestion = null;
         try {
-            personalizedQuestion = TemplateUtil.replaceSurveyResultWithJson(Prompt.PERSONALITY_PROMPT, request.surveyResultText());
+            personalizedQuestion = TemplateUtil.replaceSurveyResultWithJson(KeyWordPrompt.PERSONALITY_PROMPT, request.surveyResultText());
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -36,4 +37,5 @@ public class ChatGPTService {
         ChatGPTRequest question = new ChatGPTRequest(model, personalizedQuestion);
         return template.postForObject(apiURL, question, ChatGPTResponse.class);
     }
+
 }
