6e62f4053a64e762e90c0a2d371abd6af83557dd
Yechan Jung <skfk286@hanmail.net>
2025-01-14 18:05:49 +0900

    refactor : fcm config 설정 추가

--- src/main/java/com/doittogether/platform/common/config/FirebaseConfig.java
+++ src/main/java/com/doittogether/platform/common/config/FirebaseConfig.java
@@ -6,19 +6,18 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount  = new FileInputStream(
-                String.valueOf(ResourceUtils.getFile("src/main/resources/firebase/doittogether-e02be-firebase-adminsdk-zai51-92bd77fcfa.json"))
-        );
+        String pathToJson = Paths.get(System.getProperty("user.dir"), "doittogether-e02be-firebase-adminsdk-zai51-92bd77fcfa.json").toString();
+        FileInputStream serviceAccount  = new FileInputStream(pathToJson);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount ))
