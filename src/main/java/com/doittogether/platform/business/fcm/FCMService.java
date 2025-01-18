887d93490b9fd3e87789e32c6260a941466f29ac
Yechan Jung <skfk286@hanmail.net>
2025-01-18 22:02:21 +0900

    feat : fcm service, impl 추가

--- src/main/java/com/doittogether/platform/business/fcm/FCMService.java
+++ src/main/java/com/doittogether/platform/business/fcm/FCMService.java
@@ -1,28 +1,11 @@
 package com.doittogether.platform.business.fcm;
 
-import com.doittogether.platform.presentation.dto.fcm.PushRequest;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import com.doittogether.platform.domain.entity.User;
+import com.doittogether.platform.presentation.dto.fcm.SaveOrUpdateTokenRequest;
+import com.doittogether.platform.presentation.dto.reaction.ReactionRequest;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class FCMService {
+public interface FcmService {
 
-    private final FirebaseMessaging firebaseMessaging;
-
-    public void sendNotification(PushRequest pushRequest) {
-        try {
-            Message message = PushRequest.makeMessage(pushRequest);
-
-            String response = firebaseMessaging.send(message);
-            log.info("FCM 메세지 전송 성공: {}", response);
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("FCM 메세지 전송 실패: {}", e.getMessage());
-        }
-    }
+    void saveOrUpdateToken(User loginUser, SaveOrUpdateTokenRequest saveOrUpdateTokenRequest);
+    void sendNotification(User loginUser, ReactionRequest notificationRequest);
 }
