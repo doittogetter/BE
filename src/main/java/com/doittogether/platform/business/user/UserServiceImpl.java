5e2b88b1f3672256711c18ca49413aa919af3ddc
Seong Hoon Min <gortmtjdgns0943@gmail.com>
2025-01-22 22:19:41 +0900

    feat: 회원 서비스에 id별 provider 조회 메서드 추가

--- src/main/java/com/doittogether/platform/business/user/UserServiceImpl.java
+++ src/main/java/com/doittogether/platform/business/user/UserServiceImpl.java
@@ -14,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    // 소셜 id의 provider 위치 정보
+    private static final int PROVIDER_INDEX = 0;
+
     private final UserRepository userRepository;
 
     @Override
@@ -40,5 +43,15 @@ public class UserServiceImpl implements UserService {
     public void completeSetup(User user) {
         user.completeSetup();
         userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public String getProvider(Long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionCode.USER_NOT_FOUND));
+
+        String provider = user.getSocialId().split("_")[PROVIDER_INDEX];
+
+        return provider;
     }
 }
