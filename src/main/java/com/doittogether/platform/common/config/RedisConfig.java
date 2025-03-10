75e256c2c305d592f6df603ae55f0610972ba23e
Yechan Jung <skfk286@hanmail.net>
2025-03-10 18:28:48 +0900

    refactor : 운영 redis profile 설정 추가

--- src/main/java/com/doittogether/platform/common/config/RedisConfig.java
+++ src/main/java/com/doittogether/platform/common/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
+@Profile("prd")
 @Configuration
 public class RedisConfig {
 
